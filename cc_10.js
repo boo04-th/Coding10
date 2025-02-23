0577c167f35fc923148f244d35a7af842b11ad63
Hient <hient@usf.edu>
2025-02-23 00:02:27 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -24,3 +24,25 @@ console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, 
 prod1.updateStock(3);
 console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
 
+//Task 2: Creating an Order Class
+class Order {
+    constructor(orderId, product, quantity){ //Constructor setting up specified properties
+        if (product.stock >=quantity){
+            this.orderId = orderId;
+            this.product = product;
+            this.quantity = quantity;
+            this.totalPrice = product.price * quantity;
+            product.updateStock(quanity);
+        } else {
+            throw new Error ("Order cannot be placed due to insufficient stock.");
+        }
+    }
+    getOrderDetails() { //Add a method getOrderDetails()
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`; //Returns a formatted string of order details
+    }
+}
+
+// Test case for Task 2
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); // Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
