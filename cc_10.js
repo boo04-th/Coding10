269785c531538d2272408929295d48b83d43a074
Hient <hient@usf.edu>
2025-02-23 23:23:38 -0500

    Task 5 - Implemented Product Restocking

--- cc_10.js
+++ cc_10.js
@@ -85,3 +85,21 @@ inventory.listOrders();
 // Expected output: "Order ID: 601, Product: Laptop, Quantity: 2, Total Price: $2400"
 console.log(prod1.getDetails());
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 3"
+
+//Task 5: Implementing Product Restocking
+restockProduct(productId, quantity){
+    const product = this.products.find(prod => prod.id === productId);
+    if (product){
+        product.stock += quantity;
+        console.log(`Product ${product.name} restocked. New Stock: ${product.stock}`)
+    } else {
+        console.log("product not found");
+    }
+}
+
+//Test Case
+inventory.restockProduct(101, 5);
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 8"
+
+  
